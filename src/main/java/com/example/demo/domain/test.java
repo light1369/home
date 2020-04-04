f7e05a5ecea9deda2760a9d8809b3159f78e3d0e
light1369 <light1369@163.com>
2020-04-04 22:47:29 +0800

    第一次上传

---
+++ src/main/java/com/example/demo/domain/test.java
@@ -0,0 +1,13 @@
+package com.example.demo.domain;
+
+/**
+ * @author Duan
+ * @date 2020/4/4 - 10:58
+ */
+public class test {
+    public static void main(String[] args) {
+        
+        System.out.println(007);
+        
+    }
+}
